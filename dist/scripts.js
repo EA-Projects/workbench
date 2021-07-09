e3d1a487e1004e144588040f0b88fe19b9ce9a35
delfisav <delfina@elipseagency.com>
2021-07-09 17:24:38 -0300

    maqueta

--- dist/scripts.js
+++ dist/scripts.js
@@ -1,31 +1,56 @@
 /******/ (function() { // webpackBootstrap
-/******/ 	var __webpack_modules__ = ([
-/* 0 */,
-/* 1 */
-/***/ (function() {
-
+var __webpack_exports__ = {};
 ////// DOCUMENT READY
-$(document).ready(function () {
-	/*
+$(document).ready(function () {});
 
-	$('.column a, #back-to-top a').on('click', function (e) {
-		e.preventDefault();
-		$('html, body').animate(
-			{
-				scrollTop: $($(this).attr('href')).offset().top - 100,
-			},
-			750,
-			'swing'
-		);
-	});*/
+// Smooth scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+	event.preventDefault();
+
+	$('html, body').animate(
+		{
+			scrollTop: $($.attr(this, 'href')).offset().top,
+		},
+		500
+	);
 });
 
 ////// DOCUMENT LOAD
 $(window).on('load', function () {
-	/*$('.preloader').fadeOut(1000, function () {
-		$('body').removeClass('loading');
-	});*/
+	wow = new WOW({
+		boxClass: 'animate',
+		animateClass: 'animated',
+		offset: 200,
+		mobile: true,
+	});
+	wow.init();
+
+	// Asign day theme
+	$('html').attr('data-theme', 'morning');
+
+	// Trigger table sorting
+	$('#table th.description').css('pointer-events', 'none');
+	$('#table th.rank').click();
 });
+
+// Animate graphic when in viewport
+$(window).on('scroll', function () {
+	$('#stats').each(function () {
+		if (isScrolledIntoView($(this))) {
+			$(this).find('.progress').addClass('animate');
+		}
+	});
+});
+
+function isScrolledIntoView(elem) {
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
+
+	var elemTop = $(elem).offset().top;
+	var elemBottom = elemTop + $(elem).height();
+
+	return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
 
 ////// STICKY MENU
 $(window).scroll(function () {
@@ -52,85 +77,5 @@ if (
 	$('.animate').removeClass('animate'); // to remove transition
 }
 
-
-/***/ })
-/******/ 	]);
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	!function() {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__webpack_require__.n = function(module) {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				function() { return module['default']; } :
-/******/ 				function() { return module; };
-/******/ 			__webpack_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	!function() {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__webpack_require__.d = function(exports, definition) {
-/******/ 			for(var key in definition) {
-/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	!function() {
-/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
-/******/ 	}();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	}();
-/******/ 	
-/************************************************************************/
-var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
-!function() {
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _parts_custom_scripts_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(1);
-/* harmony import */ var _parts_custom_scripts_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_parts_custom_scripts_js__WEBPACK_IMPORTED_MODULE_0__);
-
-
-}();
 /******/ })()
 ;
--- src/scripts.js
+++ src/scripts.js
@@ -1,1 +1,76 @@
-import './js/custom-scripts.js';
+////// DOCUMENT READY
+$(document).ready(function () {});
+
+// Smooth scroll
+$(document).on('click', 'a[href^="#"]', function (event) {
+	event.preventDefault();
+
+	$('html, body').animate(
+		{
+			scrollTop: $($.attr(this, 'href')).offset().top,
+		},
+		500
+	);
+});
+
+////// DOCUMENT LOAD
+$(window).on('load', function () {
+	wow = new WOW({
+		boxClass: 'animate',
+		animateClass: 'animated',
+		offset: 200,
+		mobile: true,
+	});
+	wow.init();
+
+	// Asign day theme
+	$('html').attr('data-theme', 'morning');
+
+	// Trigger table sorting
+	$('#table th.description').css('pointer-events', 'none');
+	$('#table th.rank').click();
+});
+
+// Animate graphic when in viewport
+$(window).on('scroll', function () {
+	$('#stats').each(function () {
+		if (isScrolledIntoView($(this))) {
+			$(this).find('.progress').addClass('animate');
+		}
+	});
+});
+
+function isScrolledIntoView(elem) {
+	var docViewTop = $(window).scrollTop();
+	var docViewBottom = docViewTop + $(window).height();
+
+	var elemTop = $(elem).offset().top;
+	var elemBottom = elemTop + $(elem).height();
+
+	return elemBottom <= docViewBottom && elemTop >= docViewTop;
+}
+
+////// STICKY MENU
+$(window).scroll(function () {
+	if ($(window).scrollTop() > 20) {
+		$('#main-nav').addClass('sticky');
+	} else {
+		$('#main-nav').removeClass('sticky');
+	}
+
+	if ($(window).scrollTop() > 1500) {
+		$('#back-to-top').fadeIn();
+	} else {
+		$('#back-to-top').fadeOut();
+	}
+});
+
+////// DISABLE ANIMATIONS ON MOBILE
+if (
+	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent
+	) ||
+	$(window).width() < 575
+) {
+	$('.animate').removeClass('animate'); // to remove transition
+}
